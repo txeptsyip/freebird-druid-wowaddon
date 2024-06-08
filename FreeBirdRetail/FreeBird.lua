91b95684b0ffc66ae3a08c053b10ff6bca888d31
txeptsyip <lekoro@hotmail.co.uk>
2024-06-08 19:59:18 +0100

    now with retail and cata versions
    made a version that works with retail

---
+++ FreeBirdRetail/FreeBird.lua
@@ -0,0 +1,53 @@
+FreeBirdSavedMusic = "Interface\\AddOns\\FreeBird\\freebird.mp3"
+
+local function freeBird_Loop()
+    if IsFalling() then
+        C_Timer.After(0.5, freeBird_Loop)
+    else
+        StopMusic()
+    end
+end
+
+local AuraInstanceID
+local function testFalling()
+    if IsFalling() then
+        PlayMusic(FreeBirdSavedMusic)
+        freeBird_Loop()
+    end
+end
+
+local function freeBird_onEvent(self, event, unit, info)
+    if info.addedAuras then
+        for _, v in pairs(info.addedAuras) do
+            if v.spellId == 783 then -- druid travel form
+                AuraInstanceID = v.auraInstanceID
+                StopMusic()
+                break
+            end
+        end
+    end
+    if info.removedAuraInstanceIDs then
+        for _, v in pairs(info.removedAuraInstanceIDs) do
+            if v == AuraInstanceID then
+                AuraInstanceID = nil
+                RunNextFrame(testFalling)
+                break
+            end
+        end
+    end
+end
+
+SLASH_FREEBIRD1 = "/fb1"
+SlashCmdList["FREEBIRD"] = function(msg)
+    if msg == "original" then
+       FreeBirdSavedMusic = "Interface\\AddOns\\FreeBird\\freebird.mp3"
+    elseif msg == "harmonica" then
+       FreeBirdSavedMusic = "Interface\\AddOns\\FreeBird\\FreeBirdHarmonica.mp3"
+    else
+        print("/fb1 original, /fb1 harmonica")
+    end
+end
+
+local f = CreateFrame("Frame")
+f:RegisterUnitEvent("UNIT_AURA", "player")
+f:SetScript("OnEvent", freeBird_onEvent)
